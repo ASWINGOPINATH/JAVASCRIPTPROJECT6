578cbee3f8254b8591e2fdebef2b70b80cb98b41
aswingopinath <aswingopinath02@gmail.com>
2023-09-24 12:25:36 +0530

    first commit

---
+++ KEYOBJECTARRAY/script.js
@@ -0,0 +1,8 @@
+var foo = {
+    'alpha': 'puffin',
+    'beta': 'beagle'
+  };
+  
+  var keys = Object.keys(foo);
+  console.log(keys) // ['alpha', 'beta'] 
+  // (or maybe some other order, keys are unordered).
